fec740663041459c9f4c6b2dfb94e2edfcba0a40
Paul <psebire37@gmail.com>
2020-03-11 15:40:46 +0100

    deleted files

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -1,10 +1,6 @@
 package com.clientui.controller;
 
 import com.clientui.beans.CommandeBean;
-import com.clientui.beans.PaiementBean;
-import com.clientui.beans.ProductBean;
-import com.clientui.proxies.MicroserviceCommandeProxy;
-import com.clientui.proxies.MicroservicePaiementProxy;
 import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,8 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -1,13 +1,11 @@
 package com.clientui.proxies;
 
-import com.clientui.beans.ProductBean;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
 @FeignClient(name = "zuul-server")
 @RibbonClient(name = "microservice-produits")
