59af148d4f9a6b8c3bcc774cfe084258f60691e9
Paul <psebire37@gmail.com>
2020-03-11 15:25:42 +0100

    deleted folders

--- book/src/main/java/com/ocr/projet7/projet7/dao/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ocr.projet7.projet7.dao;
-
-import com.ocr.projet7.projet7.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookRepository extends JpaRepository<Book,Long> {
-}
--- book/src/main/java/com/ocr/projet7/projet7/dao/CopiesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ocr.projet7.projet7.dao;
-
-import com.ocr.projet7.projet7.entities.Copy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CopiesRepository extends JpaRepository<Copy,Long> {
-}
